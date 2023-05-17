6318e970e6336dd6af6e90e91d3067097bfde034
solomoncyj <solomoncyj@hotmail.com>
2023-05-17 09:03:51 +0800

    cli: add "location" and "branch" flags to be fully automatable (#40)
    Co-authored-by: V <vendicated@riseup.net>

--- cli.go
+++ cli.go
@@ -8,12 +8,26 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	"fmt"
+	"os"
 )
 
 var discords []any
+
+func isValidBranch(branch string) bool {
+	switch branch {
+	case "", "stable", "ptb", "canary", "auto":
+		return true
+	default:
+		return false
+	}
+}
+
+func die(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
 
 func main() {
 	InitGithubDownloader()
@@ -24,12 +38,21 @@ func main() {
 	var installOpenAsar = flag.Bool("install-openasar", false, "Install OpenAsar on a Discord install")
 	var uninstallOpenAsar = flag.Bool("uninstall-openasar", false, "Uninstall OpenAsar from a Discord install")
 	var updateFlag = flag.Bool("update", false, "Update your local Vencord files")
+	var locationFlag = flag.String("location", "", "Select the location of your Discord install")
+	var branchFlag = flag.String("branch", "", "Select the branch of Discord you want to modify [default|stable|ptb|canary]")
 	flag.Parse()
+
+	if *locationFlag != "" && *branchFlag != "" {
+		die("The 'location' and 'branch' flags are mutually exclusive.")
+	}
+
+	if !isValidBranch(*branchFlag) {
+		die("The 'branch' flag must be one of the following: [auto|stable|ptb|canary]")
+	}
 
 	if *installFlag || *updateFlag {
 		if !<-GithubDoneChan {
-			fmt.Println("Not", Ternary(*installFlag, "installing", "updating"), "as fetching release data failed")
-			return
+			die("Not " + Ternary(*installFlag, "installing", "updating") + " as fetching release data failed")
 		}
 	}
 
@@ -37,24 +60,24 @@ func main() {
 
 	var err error
 	if *installFlag {
-		_ = PromptDiscord("patch").patch()
+		_ = PromptDiscord("patch", *locationFlag, *branchFlag).patch()
 	} else if *uninstallFlag {
-		_ = PromptDiscord("unpatch").unpatch()
+		_ = PromptDiscord("unpatch", *locationFlag, *branchFlag).unpatch()
 	} else if *updateFlag {
 		_ = installLatestBuilds()
 	} else if *installOpenAsar {
-		discord := PromptDiscord("patch")
+		discord := PromptDiscord("patch", *locationFlag, *branchFlag)
 		if !discord.IsOpenAsar() {
 			err = discord.InstallOpenAsar()
 		} else {
-			err = errors.New("OpenAsar already installed")
+			die("OpenAsar already installed")
 		}
 	} else if *uninstallOpenAsar {
-		discord := PromptDiscord("patch")
+		discord := PromptDiscord("patch", *locationFlag, *branchFlag)
 		if discord.IsOpenAsar() {
 			err = discord.UninstallOpenAsar()
 		} else {
-			err = errors.New("OpenAsar not installed")
+			die("OpenAsar not installed")
 		}
 	} else {
 		flag.Usage()
@@ -62,15 +85,48 @@ func main() {
 
 	if err != nil {
 		fmt.Println(err)
+		os.Exit(1)
 	}
 }
 
-func PromptDiscord(action string) *DiscordInstall {
+func PromptDiscord(action, dir, branch string) *DiscordInstall {
+	if branch == "auto" {
+		for _, b := range []string{"stable", "canary", "ptb"} {
+			for _, discord := range discords {
+				install := discord.(*DiscordInstall)
+				if install.branch == b {
+					return install
+				}
+			}
+		}
+		die("No Discord install found. Try manually specifying it with the --dir flag")
+	}
+
+	if branch != "" {
+		for _, discord := range discords {
+			install := discord.(*DiscordInstall)
+			if install.branch == branch {
+				return install
+			}
+		}
+		die("Discord " + branch + " not found")
+	}
+
+	if dir != "" {
+		if discord := ParseDiscord(dir, branch); discord != nil {
+			return discord
+		} else {
+			die(dir + " is not a valid Discord install")
+		}
+	}
+
 	fmt.Println("Please choose a Discord install to", action)
+
 	for i, discord := range discords {
 		install := discord.(*DiscordInstall)
 		fmt.Printf("[%d] %s%s (%s)\n", i+1, Ternary(install.isPatched, "(PATCHED) ", ""), install.path, install.branch)
 	}
+
 	fmt.Printf("[%d] Custom Location\n", len(discords)+1)
 
 	var choice int
@@ -90,7 +146,7 @@ func PromptDiscord(action string) *DiscordInstall {
 			var custom string
 			fmt.Print("Custom Discord Install: ")
 			if _, err := fmt.Scan(&custom); err == nil {
-				if discord := ParseDiscord(custom, ""); discord != nil {
+				if discord := ParseDiscord(custom, branch); discord != nil {
 					return discord
 				}
 			}
@@ -106,5 +162,7 @@ func InstallLatestBuilds() error {
 
 func HandleScuffedInstall() {
 	fmt.Println("Hold On!")
-	fmt.Println("You have a broken Discord Install.\nPlease reinstall Discord before proceeding!\nOtherwise, Vencord will likely not work.")
+	fmt.Println("You have a broken Discord Install.")
+	fmt.Println("Please reinstall Discord before proceeding!")
+	fmt.Println("Otherwise, Vencord will likely not work.")
 }
