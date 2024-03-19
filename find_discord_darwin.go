28a8b9113ea0c5a6ac911e49779a13794a7e5c92
Leo Developer <LeoDeveloper@protonmail.com>
2024-03-19 10:54:16 +0000

    feat(MacOs): also search in $HOME/Applications for Discord (#124)

--- find_discord_darwin.go
+++ find_discord_darwin.go
@@ -7,6 +7,7 @@
 package main
 
 import (
+	"os"
 	path "path/filepath"
 	"strings"
 )
@@ -45,11 +46,17 @@ func ParseDiscord(p, branch string) *DiscordInstall {
 
 func FindDiscords() []any {
 	var discords []any
+	bases := []string{
+		"/Applications",
+		path.Join(os.Getenv("HOME"), "Applications"),
+	}
 	for branch, dirname := range macosNames {
-		p := "/Applications/" + dirname
-		if discord := ParseDiscord(p, branch); discord != nil {
-			Log.Debug("Found Discord Install at", p)
-			discords = append(discords, discord)
+		for _, base := range bases {
+			p := path.Join(base, dirname)
+			if discord := ParseDiscord(p, branch); discord != nil {
+				Log.Debug("Found Discord Install at", p)
+				discords = append(discords, discord)
+			}
 		}
 	}
 	return discords
