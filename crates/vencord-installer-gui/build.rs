48d6b8ff0ebbce727feaf2f776105b38c9c109e9
flower <97859147+khcrysalis@users.noreply.github.com>
2026-04-02 08:21:29 -0700

    manifest

--- crates/vencord-installer-gui/build.rs
+++ crates/vencord-installer-gui/build.rs
@@ -1,5 +1,29 @@
 fn main() {
     let config = slint_build::CompilerConfiguration::new().with_style("fluent".into());
-
     slint_build::compile_with_config("ui/index.slint", config).expect("Slint build failed");
+
+    #[cfg(target_os = "windows")]
+    {
+        let mut res = winresource::WindowsResource::new();
+        res.set_manifest(
+            r#"
+            <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+            <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+              <dependency>
+                <dependentAssembly>
+                  <assemblyIdentity
+                    type="win32"
+                    name="Microsoft.Windows.Common-Controls"
+                    version="6.0.0.0"
+                    processorArchitecture="*"
+                    publicKeyToken="6595b64144ccf1df"
+                    language="*"
+                  />
+                </dependentAssembly>
+              </dependency>
+            </assembly>
+            "#,
+        );
+        res.compile().expect("winresource compile failed");
+    }
 }
