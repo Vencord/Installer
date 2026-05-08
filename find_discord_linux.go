8aaad484510ba9090309b048cc9efaeab107a4f6
PeeblyWeeb <peebly@salamithecat.com>
2026-05-07 20:38:09 -0400

    (linux) Fallback to ParseDiscordNew if ParseDiscord does not find any installations

--- find_discord_linux.go
+++ find_discord_linux.go
@@ -105,7 +105,7 @@ func ParseDiscordNew(p, branch string, isFlatpak bool) *DiscordInstall {
 	}
 }
 
-func ParseDiscord(p, _ string) *DiscordInstall {
+func ParseDiscord(p, branch string) *DiscordInstall {
 	name := path.Base(p)
 
 	needsFlatpakResolve := strings.Contains(p, "/flatpak/") && !strings.Contains(p, "/current/active/files/")
@@ -130,7 +130,7 @@ func ParseDiscord(p, _ string) *DiscordInstall {
 		isPatched = ExistsFile(path.Join(p, "_app.asar.unpacked"))
 	} else {
 		// Log.Warn("Tried to parse invalid Location:", p)
-		return nil
+		return ParseDiscordNew(p, branch, needsFlatpakResolve)
 	}
 
 	return &DiscordInstall{
